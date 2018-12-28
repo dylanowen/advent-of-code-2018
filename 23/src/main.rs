bee0594f9347e92a9613fa7b4681e8e276a990ce
Dylan Owen <dylan.owen@workday.com>
2018-12-28 13:59:23 -0800

    completed 23

--- 23/src/main.rs
+++ 23/src/main.rs
@@ -1,32 +1,11 @@
 use regex::Regex;
 use std::option::Option::{Some, None};
-use std::ops::Range;
+use std::cmp::Ordering;
+
+use priority_queue::PriorityQueue;
 
 use common::*;
 
-//#[derive(Copy, Clone, PartialEq, Eq, Debug)]
-//enum Attack {
-//   Fire,
-//   Slashing,
-//   Bludgeoning,
-//   Cold,
-//   Radiation,
-//}
-//
-//#[derive(Copy, Clone, PartialEq, Eq, Debug)]
-//enum Army {
-//   Immune,
-//   Infection,
-//}
-//
-//const ATTACKS: [Attack; 5] = [
-//   Attack::Fire,
-//   Attack::Slashing,
-//   Attack::Bludgeoning,
-//   Attack::Cold,
-//   Attack::Radiation,
-//];
-//
 #[derive(Copy, Clone, PartialEq, Eq, Debug)]
 struct NanoBot {
    location: Coordinate,
@@ -36,8 +15,21 @@ struct NanoBot {
 #[derive(Copy, Clone, PartialEq, Eq, Debug)]
 struct Coordinate(i64, i64, i64);
 
-#[derive(Copy, Clone, PartialEq, Eq, Debug)]
+#[derive(Copy, Clone, PartialEq, Eq, Hash, Debug)]
 struct CoordRange(i64, i64);
+
+#[derive(Copy, Clone, PartialEq, Eq, Hash, Debug)]
+struct Region {
+   x: CoordRange,
+   y: CoordRange,
+   z: CoordRange,
+}
+
+#[derive(Copy, Clone, PartialEq, Eq, Debug)]
+struct RegionScore {
+   potential_bots: usize,
+   zero_distance: i64,
+}
 
 const ZERO_COORDINATE: Coordinate = Coordinate(0, 0, 0);
 
@@ -62,20 +54,21 @@ fn main() {
          .collect()
    }
 
-//   run_tests("23", "test_input_{}.txt",
-//             vec![36],
-//             &|contents| {
-//                let nanobots = parse_input(contents);
-//
-//                b2(&nanobots)
-//             },
-//   );
+   run_tests("23", "test_input_{}.txt",
+             vec![36],
+             &|contents| {
+                let nanobots = parse_input(contents);
+
+                b(&nanobots)
+             },
+   );
 
    run_day("23", &|contents, is_sample| {
       let nanobots = parse_input(contents);
 
       let a_result = a(&nanobots);
       println!("Result A: {}", a_result);
+
 
       if is_sample {
          assert_eq!(7, a_result);
@@ -107,43 +100,6 @@ fn a(nanobots: &Vec<NanoBot>) -> isize {
    in_range
 }
 
-fn b2(nanobots: &Vec<NanoBot>) -> i64 {
-   // narrow down our search area
-   let mut groups: Vec<Vec<NanoBot>> = vec![];
-
-   for bot in nanobots.iter() {
-      let mut any_overlap = false;
-
-      for i in 0..groups.len() {
-         let mut group_overlap = false;
-         for other in groups[i].iter() {
-            if bot.distance(&other) <= bot.radius + other.radius {
-               group_overlap = true;
-               break;
-            }
-         }
-         groups[i].push(bot.clone());
-
-         any_overlap = any_overlap | group_overlap;
-      }
-
-      if !any_overlap {
-         groups.push(vec![bot.clone()])
-      }
-   }
-
-   for group in groups.iter() {
-      println!("len: {}", group.len());
-      for bot in group.iter() {
-         println!("\t{:?}", bot);
-      }
-      println!();
-   }
-
-   println!("{:?}", groups);
-   0
-}
-
 fn b(nanobots: &Vec<NanoBot>) -> i64 {
    let mut min: Coordinate = Coordinate::max_value();
    let mut max: Coordinate = Coordinate::min_value();
@@ -160,161 +116,48 @@ fn b(nanobots: &Vec<NanoBot>) -> i64 {
 
    let (outer_x_range, outer_y_range, outer_z_range) = min.ranges(&max);
 
-   find_closest(nanobots, &outer_x_range, &outer_y_range, &outer_z_range, 0, 0).unwrap().0
-      .distance(&ZERO_COORDINATE)
+   let region = Region {
+      x: outer_x_range,
+      y: outer_y_range,
+      z: outer_z_range,
+   };
+
+   find_closest(nanobots, &region).unwrap().distance(&ZERO_COORDINATE)
 }
 
-fn find_closest(nanobots: &Vec<NanoBot>,
-                outer_x_range: &CoordRange,
-                outer_y_range: &CoordRange,
-                outer_z_range: &CoordRange,
-                initial_max: usize,
-                debug_offset: usize) -> Option<(Coordinate, usize)> {
-   let debug_prefix = "  ".repeat(debug_offset);
-
-   let max_search_area = outer_x_range.distance() as u128 * outer_y_range.distance() as u128 * outer_z_range.distance() as u128;
-   if max_search_area < 1_000 {
-      return simple_find_closest(nanobots, outer_x_range, outer_y_range, outer_z_range, initial_max, debug_offset);
-   }
-
-   let mut max_in_range = initial_max;
-   let mut max_ranges: Vec<(CoordRange, CoordRange, CoordRange)> = vec![];
-
-   for x_range in outer_x_range.split().iter() {
-      // don't search if the range doesn't make sense
-      if !x_range.is_logical() {
-         continue;
-      }
-      let x_mid = x_range.mid();
-
-      for y_range in outer_y_range.split().iter() {
-         // don't search if the range doesn't make sense
-         if !y_range.is_logical() {
-            continue;
-         }
-         let y_mid = y_range.mid();
-
-         for z_range in outer_z_range.split().iter() {
-            // don't search if the range doesn't make sense
-            if !z_range.is_logical() {
-               continue;
-            }
-
-            // where we are searching:
-            let center = Coordinate(x_mid, y_mid, z_range.mid());
-
-            let tolerance = ((x_range.distance() / 2) + 1)
-               + ((y_range.distance() / 2) + 1)
-               + ((z_range.distance() / 2) + 1);
-
-            let mut in_range = 0;
-            for bot in nanobots.iter() {
-               //println!("{:?} {}", bot, center.distance(&bot.location));
-
-               if center.distance(&bot.location) <= bot.radius + tolerance {
-                  in_range += 1;
-               }
-            }
-
-            if in_range > max_in_range {
-               max_in_range = in_range;
-               max_ranges = vec![(x_range.clone(), y_range.clone(), z_range.clone())];
-
-               println!("{}new max: {} {:?}", debug_prefix, max_in_range, (x_range.clone(), y_range.clone(), z_range.clone()));
-            } else if in_range == max_in_range {
-               max_ranges.push((x_range.clone(), y_range.clone(), z_range.clone()));
-
-               //println!("{}other max: {:?}", debug_prefix, (x_range.clone(), y_range.clone(), z_range.clone()));
-            }
-
-            println!("in_range: {} {:?} {}", in_range, center, tolerance);
-         }
-      }
-   }
-
-   return None;
-
-   let mut single_max = initial_max;
-   let mut single_points: Vec<(Coordinate, usize)> = vec![];
-
-   if max_ranges.len() > 0 {
-      //println!("{}found: {:?}", debug_prefix, max_ranges);
-   }
-
-   // search the other ranges
-   for range in max_ranges {
-      //println!("{}Stepping into {:?} {:?} {:?} {} {} {}", debug_prefix, range.0, range.1, range.2, (range.0).distance(), (range.1).distance(), (range.2).distance());
-
-      match find_closest(nanobots, &range.0, &range.1, &range.2, single_max, debug_offset + 1) {
-         Some(closest) => {
-            if closest.1 > single_max {
-               single_max = closest.1;
-               single_points = vec![closest];
-
-               println!("{}new_max: {} @ {:?}", debug_prefix, single_max, closest);
-            } else if closest.1 == single_max {
-               single_points.push(closest);
-            }
-         }
-         None => {}
-      }
-   }
-   //println!("{}done", debug_prefix);
-
-   single_points.sort_unstable_by(|left, right| {
-      right.1.cmp(&left.1)
-         .then_with(|| {
-            left.0.distance(&ZERO_COORDINATE)
-               .cmp(&right.0.distance(&ZERO_COORDINATE))
-         })
+fn find_closest(nanobots: &Vec<NanoBot>, entire_region: &Region) -> Option<Coordinate> {
+   let mut candidates: PriorityQueue<Region, RegionScore> = PriorityQueue::new();
+   candidates.push(entire_region.clone(), RegionScore {
+      potential_bots: entire_region.potential_bots(nanobots),
+      zero_distance: entire_region.zero_distance(),
    });
 
-   //println!("{:?}", single_points);
+   while !candidates.is_empty() {
+      //let mut i = 0;
+      //for (region, score) in candidates.clone().into_sorted_iter() {
+      //   if i > 20 {
+      //      break;
+      //   }
+      //   println!("{:?} {:?} ", region._area(), score);
+      //   i += 1;
+      //}
+      //println!();
 
-   single_points.first().map(|p| p.clone())
-}
+      let (region, _) = candidates.pop().unwrap();
 
-fn simple_find_closest(nanobots: &Vec<NanoBot>,
-                       x_range: &CoordRange,
-                       y_range: &CoordRange,
-                       z_range: &CoordRange,
-                       initial_max: usize,
-                       debug_offset: usize) -> Option<(Coordinate, usize)> {
-   let debug_prefix = "  ".repeat(debug_offset);
-   //println!("{} simple find over: {}", debug_prefix, x_range.distance() * y_range.distance() * z_range.distance());
+      if region.x.distance() == 1 && region.y.distance() == 1 && region.z.distance() == 1 {
+         return Some(region.lower_bound());
+      }
 
-   let mut max_in_range = initial_max;
-   let mut max_coord = Coordinate(0, 0, 0);
-   let mut found = false;
-
-   for x in x_range.range() {
-      for y in y_range.range() {
-         for z in z_range.range() {
-            let current = Coordinate(x, y, z);
-            let mut in_range = 0;
-            for bot in nanobots.iter() {
-               if bot.location.distance(&current) <= bot.radius {
-                  in_range += 1;
-               }
-            }
-
-            if in_range > max_in_range || (
-               in_range == max_in_range &&
-                  ZERO_COORDINATE.distance(&current) < ZERO_COORDINATE.distance(&max_coord)
-            ) {
-               max_in_range = in_range;
-               max_coord = current.clone();
-               found = true;
-            }
-         }
+      for split_region in region.split() {
+         candidates.push(split_region, RegionScore {
+            potential_bots: split_region.potential_bots(nanobots),
+            zero_distance: split_region.zero_distance(),
+         });
       }
    }
 
-   if found {
-      Some((max_coord, max_in_range))
-   } else {
-      None
-   }
+   None
 }
 
 impl NanoBot {
@@ -323,26 +166,95 @@ impl NanoBot {
    }
 }
 
-impl CoordRange {
-   fn is_logical(&self) -> bool {
-      self.1 >= self.0
+impl Region {
+   fn split(&self) -> Vec<Region> {
+      let mut split_regions = vec![];
+      for x in self.x.split().iter() {
+         if x.distance() == 0 {
+            continue;
+         }
+         for y in self.y.split().iter() {
+            if y.distance() == 0 {
+               continue;
+            }
+            for z in self.z.split().iter() {
+               if z.distance() == 0 {
+                  continue;
+               }
+
+               split_regions.push(Region {
+                  x: x.clone(),
+                  y: y.clone(),
+                  z: z.clone(),
+               })
+            }
+         }
+      }
+
+      split_regions
    }
 
+   fn potential_bots(&self, nanobots: &Vec<NanoBot>) -> usize {
+      let center = Coordinate(self.x.mid(), self.y.mid(), self.z.mid());
+
+      // if this divides evenly our center is offset, so give us some more room
+      let tolerance = (self.x.distance() / 2) + if self.x.distance() % 2 == 0 { 1 } else { 0 } +
+         (self.y.distance() / 2) + if self.y.distance() % 2 == 0 { 1 } else { 0 } +
+         (self.z.distance() / 2) + if self.z.distance() % 2 == 0 { 1 } else { 0 };
+
+      let mut in_range = 0;
+      for bot in nanobots.iter() {
+         if center.distance(&bot.location) <= bot.radius + tolerance {
+            in_range += 1;
+         }
+      }
+
+      in_range
+   }
+
+   fn zero_distance(&self) -> i64 {
+      let mut lowest = i64::max_value();
+      for x in &[self.x.0, self.x.1] {
+         for y in &[self.y.0, self.y.1] {
+            for z in &[self.z.0, self.z.1] {
+               let distance = ZERO_COORDINATE.distance(&Coordinate(*x, *y, *z));
+
+               if distance < lowest {
+                  lowest = distance;
+               }
+            }
+         }
+      }
+
+      lowest
+   }
+
+   fn _area(&self) -> i128 {
+      self.x.distance() as i128 * self.y.distance() as i128 * self.z.distance() as i128
+   }
+
+   fn lower_bound(&self) -> Coordinate {
+      Coordinate(self.x.0, self.y.0, self.z.0)
+   }
+}
+
+impl Ord for RegionScore {
+   fn cmp(&self, other: &RegionScore) -> Ordering {
+      self.potential_bots.cmp(&other.potential_bots)
+         .then_with(|| {
+            self.zero_distance.cmp(&other.zero_distance)
+         })
+   }
+}
+
+impl PartialOrd for RegionScore {
+   fn partial_cmp(&self, other: &RegionScore) -> Option<Ordering> {
+      Some(self.cmp(other))
+   }
+}
+
+impl CoordRange {
    fn split(&self) -> [CoordRange; 2] {
-//      println!("{:?}", [
-//         Range(self.0, self.mid()),
-//         Range(self.mid(), self.1),
-//      ]);
-
-//      let left = CoordRange(self.0, self.mid());
-//      let right = CoordRange(self.mid(), self.1);
-//      [
-//         CoordRange(left.0, left.mid()),
-//         CoordRange(left.mid(), left.1),
-//         CoordRange(right.0, right.mid()),
-//         CoordRange(right.mid(), right.1),
-//      ]
-
       [
          CoordRange(self.0, self.mid()),
          CoordRange(self.mid(), self.1),
@@ -350,17 +262,11 @@ impl CoordRange {
    }
 
    fn mid(&self) -> i64 {
-      //println!("mid: {} {} {}", self.0, self.1, (self.1 - self.0) / 2 + self.0);
-
       (self.1 - self.0) / 2 + self.0
    }
 
    fn distance(&self) -> i64 {
       self.1 - self.0
-   }
-
-   fn range(&self) -> Range<i64> {
-      self.0..self.1
    }
 }
 
